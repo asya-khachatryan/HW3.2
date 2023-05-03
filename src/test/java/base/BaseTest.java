2fc76510fb6ee60ec04f589902e9bc073529f06e
asya-khachatryan <asya_khachatryan2@edu.aua.am>
2023-05-04 00:06:07 +0400

    [HW3.3] Implemented Remote Web Driver

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -4,8 +4,12 @@ import com.google.common.io.Files;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariOptions;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
@@ -15,6 +19,8 @@ import pages.AmazonHomePage;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -25,12 +31,13 @@ public class BaseTest {
     protected AmazonHomePage homePage;
 
     @BeforeClass
-    public void initWebDriver() {
-        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver");
+    public static void initWebDriver() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-        options.addArguments("--incognito");
-        driver = new ChromeDriver(options);
+        try {
+            driver = new RemoteWebDriver(new URL("http://192.168.10.21:4444"), options);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
     }
